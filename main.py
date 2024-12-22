cafcaa4c70f1321b8927d5679835e10b67d94464
Вадим Голов <grafit-design@inbox.ru>
2024-12-22 20:24:59 +0500

    Подправил кавычки

--- main.py
+++ main.py
@@ -10,14 +10,13 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from pyexpat.errors import messages
 
 from loader import bot, dp, logger
 
-button_registry = KeyboardButton(text="Регистрация в телеграм боте")
-button_exchange_rates = KeyboardButton(text="Курс валют")
-button_tips = KeyboardButton(text="Советы по экономии")
-button_finances = KeyboardButton(text="Личные финансы")
+button_registry = KeyboardButton(text='Регистрация в телеграм боте')
+button_exchange_rates = KeyboardButton(text='Курс валют')
+button_tips = KeyboardButton(text='Советы по экономии')
+button_finances = KeyboardButton(text='Личные финансы')
 
 keyboards = ReplyKeyboardMarkup(keyboard=[
     [button_registry, button_exchange_rates],
@@ -54,22 +53,22 @@ class FinancesForm(StatesGroup):
 
 @dp.message(Command('start'))
 async def send_start(message: Message):
-    await message.answer("Привет! Я ваш личный финансовый помощник. Выберите одну из опций в меню:", reply_markup=keyboards)
+    await message.answer('Привет! Я ваш личный финансовый помощник. Выберите одну из опций в меню:', reply_markup=keyboards)
     await registration(message=message)
 
 
-@dp.message(F.text == "Регистрация в телеграм боте")
+@dp.message(F.text == 'Регистрация в телеграм боте')
 async def registration(message: Message):
     telegram_id = message.from_user.id
     name = message.from_user.full_name
     cursor.execute('''SELECT * FROM users WHERE telegram_id = ?''', (telegram_id,))
     user = cursor.fetchone()
     if user:
-        await message.answer("Вы уже зарегистрированы!")
+        await message.answer('Вы уже зарегистрированы!')
     else:
         cursor.execute('''INSERT INTO users (telegram_id, name) VALUES (?, ?)''', (telegram_id, name))
         conn.commit()
-        await message.answer("Вы успешно зарегистрированы!")
+        await message.answer('Вы успешно зарегистрированы!')
 
 
 @dp.message(F.text == "Курс валют")
@@ -79,7 +78,7 @@ async def exchange_rates(message: Message):
         response = requests.get(url)
         data = response.json()
         if response.status_code != 200:
-            await message.answer("Не удалось получить данные о курсе валют!")
+            await message.answer('Не удалось получить данные о курсе валют!')
             return
         usd_to_rub = data['conversion_rates']['RUB']
         eur_to_usd = data['conversion_rates']['EUR']
@@ -131,45 +130,45 @@ async def send_tips(message: Message):
     await message.answer(tip)
 
 
-@dp.message(F.text == "Личные финансы")
+@dp.message(F.text == 'Личные финансы')
 async def finances(message: Message, state: FSMContext):
     await state.set_state(FinancesForm.category1)
-    await message.reply("Введите первую категорию расходов:")
+    await message.reply('Введите первую категорию расходов:')
 
 
 @dp.message(FinancesForm.category1)
 async def finances(message: Message, state: FSMContext):
     await state.update_data(category1=message.text)
     await state.set_state(FinancesForm.expenses1)
-    await message.reply("Введите расходы для категории 1:")
+    await message.reply('Введите расходы для категории 1:')
 
 
 @dp.message(FinancesForm.expenses1)
 async def finances(message: Message, state: FSMContext):
     await state.update_data(expenses1=float(message.text))
     await state.set_state(FinancesForm.category2)
-    await message.reply("Введите вторую категорию расходов:")
+    await message.reply('Введите вторую категорию расходов:')
 
 
 @dp.message(FinancesForm.category2)
 async def finances(message: Message, state: FSMContext):
     await state.update_data(category2=message.text)
     await state.set_state(FinancesForm.expenses2)
-    await message.reply("Введите расходы для категории 2:")
+    await message.reply('Введите расходы для категории 2:')
 
 
 @dp.message(FinancesForm.expenses2)
 async def finances(message: Message, state: FSMContext):
     await state.update_data(expenses2=float(message.text))
     await state.set_state(FinancesForm.category3)
-    await message.reply("Введите третью категорию расходов:")
+    await message.reply('Введите третью категорию расходов:')
 
 
 @dp.message(FinancesForm.category3)
 async def finances(message: Message, state: FSMContext):
     await state.update_data(category3=message.text)
     await state.set_state(FinancesForm.expenses3)
-    await message.reply("Введите расходы для категории 3:")
+    await message.reply('Введите расходы для категории 3:')
 
 
 @dp.message(FinancesForm.expenses3)
@@ -180,8 +179,7 @@ async def finances(message: Message, state: FSMContext):
                    (data['category1'], data['expenses1'], data['category2'], data['expenses2'], data['category3'], float(message.text), telegarm_id))
     conn.commit()
     await state.clear()
-
-    await message.answer("Категории и расходы сохранены!")
+    await message.answer('Категории и расходы сохранены!')
 
 
 @logger.catch
@@ -196,9 +194,8 @@ async def stop_bot() -> None:
 
 
 if __name__ == '__main__':
-    if __name__ == '__main__':
-        try:
-            asyncio.run(main())
+    try:
+        asyncio.run(main())
 
-        except KeyboardInterrupt:
-            asyncio.run(stop_bot())
+    except KeyboardInterrupt:
+        asyncio.run(stop_bot())
